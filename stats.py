88644912f0c8f98eca3752f94eafd76087794833
ipo-exe <possantti@gmail.com>
2021-11-03 22:09:35 -0300

    fitting gamma in syn

--- stats.py
+++ stats.py
@@ -1,4 +1,4 @@
-"""
+'''
 ***** UNIVERSIDADE FEDERAL DO RIO GRANDE DO SUL *****
 ********** GESPLA IPH/UFGRS PYTHON LIBRARY **********
 
@@ -29,13 +29,21 @@ Ipora Possantti: https://github.com/ipo-exe
 
 First commit: 11 of November of 2021
 
-"""
+'''
 
 import numpy as np
 import pandas as pd
 
-def fit_cdf_gamma(obs_df, val_field='Values', p_field='Percentiles', iterations=10,
-                  size=20, a_min=0.1, a_max=10, s_min=1, s_max=100):
+
+def fit_cdf_gamma(obs_df,
+                  val_field='Values',
+                  p_field='Percentiles',
+                  iterations=10,
+                  gridsize=20,
+                  shape_min=0.1,
+                  shape_max=10,
+                  scale_min=0.0,
+                  scale_max=100):
     """
 
     Fit a CDF gamma distribution by incremental grid sampling
@@ -44,40 +52,57 @@ def fit_cdf_gamma(obs_df, val_field='Values', p_field='Percentiles', iterations=
     :param val_field: string values field
     :param p_field: string percentiles field
     :param iterations: int number of iterations
-    :param size: int size of grid
-    :param a_min: float
-    :param a_max: float
-    :param s_min: float
-    :param s_max: float
+    :param gridsize: int size of grid
+    :param shape_min: float
+    :param shape_max: float
+    :param scale_min: float
+    :param scale_max: float
     :return: dict
     """
     from scipy.stats import gamma
     from analyst import rmse
+    #
+    # sampling loop:
     for t in range(iterations):
+        # starting bounding box
         if t == 0:
-            _a = np.linspace(start=a_min, stop=a_max, num=size)
-            _s = np.linspace(start=s_min, stop=s_max, num=size)
+            _a = np.linspace(start=shape_min, stop=shape_max, num=gridsize)
+            _s = np.linspace(start=scale_min, stop=scale_max, num=gridsize)
+        # zooming bounding box
         else:
-            _a = np.linspace(start=df_3['a'].min(), stop=df_3['a'].max(), num=size)
-            _s = np.linspace(start=df_3['s'].min(), stop=df_3['s'].max(), num=size)
+            _a = np.linspace(start=_df['shape'].min(), stop=_df['shape'].max(), num=gridsize)
+            _s = np.linspace(start=_df['scale'].min(), stop=_df['scale'].max(), num=gridsize)
+        #
         # deploy lists
         _x = list()
         _y = list()
         _z = list()
+        #
         # loop in parameter space
         for i in range(len(_a)):
             for j in range(len(_s)):
                 lcl_a = _a[i]
                 lcl_s = _s[j]
+                #
                 # compute the model simulation
                 lcl_sim = gamma.cdf(obs_df[val_field], lcl_a, scale=lcl_s)
+                #
                 # get model metric
                 lcl_rmse = rmse(obs=obs_df[p_field].values, sim=lcl_sim)
+                #
+                # append
                 _x.append(lcl_a)
                 _y.append(lcl_s)
                 _z.append(lcl_rmse)
-        # insert in dataframe
-        df_2 = pd.DataFrame({'a': _x, 's': _y, 'RMSE': _z})
+        #
+        # insert lists in dataframe
+        _aux_df = pd.DataFrame({'shape': _x, 'scale': _y, 'RMSE': _z})
+        #
         # select top 10
-        df_3 = df_2.sort_values(by='RMSE', ascending=True).head(10)
-    return {'a': df_3['a'].values[0], 's': df_3['s'].values[0], 'RMSE': df_3['RMSE'].values[0]}
+        _df = _aux_df.sort_values(by='RMSE', ascending=True).head(10)
+    #
+    # output dict
+    out_dct = {'shape': _df['shape'].values[0],
+               'scale': _df['scale'].values[0],
+               'RMSE': _df['RMSE'].values[0]}
+    return out_dct
--- syn.py
+++ syn.py
@@ -34,6 +34,7 @@ First commit: 11 of November of 2021
 import numpy as np
 import pandas as pd
 import resample
+import stats
 import tsa
 import matplotlib.pyplot as plt
 
@@ -105,13 +106,38 @@ def synthetic_states(states, t_matrix, size=1000):
 
 def synthetic_values(states_series, cdfs, states):
     """
+
     Generate a synthetic series based on states and respective Cumulative Frequency Curve (CFC)
+
     :param states_series: 1d numpy array series of states (integer values)
-    :param cdfs: dict of cfcs. Keys must be the string version of the states
+    :param cdfs: dict of dict cumulative density function parameters.
+
+    Upper keys must be the string version of the states
+
+    Lower keys allowed:
+    'kind' -- string type of distribution. options:
+    'constant', 'gamma'
+    parameters keys:
+    'c' -- contant value for 'constant'
+    'shape' -- shape parameter for 'gamma'
+    'scale' -- scale parameter for 'gamma'
+    'loc' -- central location value for 'gamma'
+
+    example:
+
+    {'0':{'kind':'constant', 'c': 0},
+    '1':{'kind':'gamma', 'shape': 1.3, 'scale': 2, 'loc': 0}}
+
     :param states: 1d numpy array of unique states (integer values)
     :return: 1d numpy array of synthetic series
     """
     from datetime import datetime
+    # loop in states for import efficiency
+    for s in states:
+        if cdfs[str(s)]['kind'] == 'constant':
+            pass
+        elif cdfs[str(s)]['kind'] == 'gamma':
+            from scipy.stats import gamma
     # deploy random state
     seed = int(str(datetime.now())[-6:])
     np.random.seed(seed)
@@ -119,11 +145,23 @@ def synthetic_values(states_series, cdfs, states):
     syn_values = np.zeros(shape=len(states_series))
     # loop in states
     for s in states:
-        # loop in series
-        for t in range(len(states_series)):
-            if states_series[t] == s:
-                # random choice in cfc
-                syn_values[t] = np.random.choice(a=cdfs[str(s)], size=1)[0]
+        # generate a boolean for the state
+        _boolean_state = 1 * (states_series == s)
+        if cdfs[str(s)]['kind'] == 'constant':
+            # get values
+            _values = cdfs[str(s)]['c'] * np.ones(shape=len(states_series))
+            # add up to full series:
+            syn_values = syn_values + (_boolean_state * _values)
+        elif cdfs[str(s)]['kind'] == 'gamma':
+            # get probs
+            _rand_p = np.random.random(size=len(states_series))
+            # get values in the inverse function
+            _values = gamma.ppf(q=_rand_p,
+                                a=cdfs[str(s)]['shape'],
+                                scale=cdfs[str(s)]['scale'],
+                                loc=cdfs[str(s)]['loc'])
+            # add up to full series:
+            syn_values = syn_values + (_boolean_state * _values)
     return syn_values
 
 
@@ -143,6 +181,7 @@ def syn_prec(dataframe, date_field='Date', var_field='Prec', start_date='1970-01
     - Prec_syn
     - Prec_obs
     """
+    #
     # deploy syn series
     syn_df = pd.DataFrame()
     syn_df['Date'] = pd.date_range(start=start_date, end=end_date, freq='D')
@@ -159,30 +198,47 @@ def syn_prec(dataframe, date_field='Date', var_field='Prec', start_date='1970-01
     month_dct = resample.group_by_month(dataframe=series_df, var_field=var_field)
     months_lst = list(month_dct.keys())
     #
-    # compute the CFCs for each month
+    # compute the CFCs parameters for each month
     cdfs_dct = dict()
     for i in range(len(months_lst)):
-        lcl_freq = tsa.frequency(dataframe=month_dct[months_lst[i]], var_field=var_field, zero=False, step=0.1)
-
-        if i == 0:
-            lcl_freq.to_csv('C:/bin/freq.txt', sep=';', index=False)
-            cdfs_dct['Exceedance'] = lcl_freq['Exceedance'].values
-        cdfs_dct[months_lst[i]] = lcl_freq['Values'].values
-    cdfs_df = pd.DataFrame(cdfs_dct)
+        #
+        # compute observed frequency dataframe
+        lcl_freq = tsa.frequency(dataframe=month_dct[months_lst[i]], var_field=var_field, zero=False, step=1)
+        #
+        # convert to probability:
+        lcl_freq['Percentiles'] = lcl_freq['Percentiles'] / 100
+        # remove upper and lower lines
+        #
+        # get fitted parameters
+        print('fitting Gamma distribution ...')
+        lcl_params = stats.fit_cdf_gamma(obs_df=lcl_freq,
+                                         val_field='Values',
+                                         p_field='Percentiles')
+        print('Fitted parameters: {}'.format(lcl_params))
+        cdfs_dct[months_lst[i]] = lcl_params
     #
     # compute a synthetic series for each month:
     syn_months = dict()
     for m in months_lst:
         lcl_prec = month_dct[m]['Prec'].values
+        #
         # convert the array to a boolean array to get the rain states
-        states_prec = (lcl_prec > 0) * 1
+        rain_states = (lcl_prec > 0) * 1  # 0 = dry, 1 = wet
+        #
         # compute the transition matrix
-        t_matrix, un_states = transition_matrix(states_series=states_prec)
+        t_matrix, un_states = transition_matrix(states_series=rain_states)
+        #
         # compute the synthetic series of states
         lcl_syn_states = synthetic_states(states=un_states, t_matrix=t_matrix, size=size_months)
         #
         # built dict of states CFCs:
-        lcl_cdf = {'0': cdfs_df[m].values * 0, '1': cdfs_df[m].values}
+        lcl_cdf = {'0': {'kind': 'constant',
+                         'c': 0},
+                   '1': {'kind': 'gamma',
+                         'shape': cdfs_dct[m]['shape'],
+                         'scale': cdfs_dct[m]['scale'],
+                         'loc': 0}}
+        #
         # assign values to the states
         lcl_syn = synthetic_values(states_series=lcl_syn_states, cdfs=lcl_cdf, states=un_states)
         syn_months[m] = lcl_syn
